800cada2b4fae310db52b96918cadf7c30bb30de
riqsans <thrq88@gmail.com>
2024-05-10 13:28:19 +0700

    membuat class node

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 using namespace std;
+
+// Node class representing a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
