2dce92e3f97227d5948be9915e03508f9ba5cd84
riqsans <thrq88@gmail.com>
2024-05-10 13:33:57 +0700

    menuliskan cout push value

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -12,3 +12,24 @@ public:
         next = NULL;
     }
 };
+
+// Stack class
+class Stack {
+private:
+    Node* top; // Pointer to the top node of the stack
+
+
+public:
+    Stack() {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+
+    // Push operation: Insert an element onto the top of the stack
+    int push(int value) {
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top;
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+    }
+}
